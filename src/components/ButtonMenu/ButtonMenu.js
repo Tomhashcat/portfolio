0e758cc956e9fbc6c2bca055c9b1ae15946920b2
Tomhashcat <dutourne.t@gmail.com>
2023-12-21 14:08:30 +0100

    :first push

---
+++ src/components/ButtonMenu/ButtonMenu.js
@@ -0,0 +1,64 @@
+
+import React, {useEffect} from "react";
+import { Link } from "react-router-dom";
+import "./Button.scss";
+
+function ButtonMenu(){
+    useEffect(() => {
+        let toggle = document.querySelector('.toggle');
+        let menu = document.querySelector('.menu');
+    
+        const handleClick = () => {
+          menu.classList.toggle('active');
+        };
+    
+        toggle.addEventListener('click', handleClick);
+    
+        // Cleanup the event listener when the component unmounts
+        return () => {
+          toggle.removeEventListener('click', handleClick);
+        };
+      }, []);
+return(
+    <div className="Animated-Radial-Menu">
+
+    <ul className="menu">
+        <div className="toggle"><ion-icon className="add-outline"><h1>Menu</h1></ion-icon></div>
+        <li style={{"--i": 0,"--clr" : "#ff2972"}}>
+            <Link to="#">
+                <ion-icon name="beer-outline"><h1>Menu</h1></ion-icon>
+            </Link>
+        </li>
+        <li style={{"--i": 1,"--clr" :" #fee800"}}>
+            <Link to="#">
+                <ion-icon name="home-outline"><h1>Menu</h1></ion-icon>
+            </Link>
+        </li>
+        <li style={{"--i": 2," --clr":" #04fc4;"}}>
+            <Link to="#">
+                <ion-icon name="person-outline"></ion-icon>
+            </Link>
+        </li>
+        <li style={{"--i": 3, "--clr":" #fe00f1"}}>
+            <Link to="#">
+                <ion-icon name="settings-outline"><h1>Menu</h1></ion-icon>
+            </Link>
+        </li>
+        <li style={{"--i": 4," --clr":" #00b0fe"}}>
+            <Link to="#">
+                <ion-icon name="mail-outline"></ion-icon>
+            </Link>
+        </li>
+     
+    </ul>
+      <p>loremisp
+        
+    </p>
+
+</div>
+
+
+)
+}
+
+export default ButtonMenu;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss";
+import ButtonMenu from "../../components/ButtonMenu/ButtonMenu";
+
+function Home(){
+    return(
+<>
+<div>
+    <ButtonMenu/>
+</div>
+</>    )
+}
+
+export default Home;
