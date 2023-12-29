0c5e4de88b48128108242077ff8537faae11a29b
Tomhashcat <dutourne.t@gmail.com>
2023-12-29 14:49:58 +0100

    :build snippets

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Error from './pages/404/Error';
 
+function LocalBusinessScript() {
+  const scriptContent = {
+    "@context": "http://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Thomas Dutourné",
+    "description": "Photographe professionnelle",
+    "telephone": "0661906998"
+  };
+
+  return (
+    <script type="application/ld+json">
+      {JSON.stringify(scriptContent)}
+    </script>
+  );
+}
 
 function App() {
   return (
@@ -15,10 +30,10 @@ function App() {
             <Routes>
               <Route path="/*" element={<Home />} />
               
-              <Route path="/*" element={<Error />} />
+              <Route path="*" element={<Error />} />
                </Routes> 
           </main>
-         
+        <LocalBusinessScript />
         </div>
      
     </Router>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,11 +8,18 @@ import BankPage from "../Bank/Bank";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 function Home() {
   return (
     <>
-      <h1>Thomas Dutourné</h1>
-      <h2>Personal portfolio for web developper</h2>
+
+        <title>Thomas Dutourné - Web Developer</title>
+        <meta name="description" content="Personal portfolio for web developer. Specialized in ReactJS and JavaScript." />
+        <meta property="og:title" content="Thomas Dutourné - Web Developer" />
+        <meta property="og:description" content="Personal portfolio for web developer. Specialized in ReactJS and JavaScript." />
+        <meta property="og:image" content="https://your-website-url.com/og-image.jpg" />
+        <meta property="og:url" content="https://your-website-url.com" />
+     
       <h3>Modern and créative</h3>
       <a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit" aria-label="LinkedIn Profile">
         <FontAwesomeIcon className="icones" alt='icone linkedin' icon={faLinkedin} />
@@ -30,10 +37,9 @@ function Home() {
             <p>
               Diplômé en développement front-end, je maîtrise ReactJS et
               JavaScript. Spécialisé dans la création d'interfaces réactives et
-              élégantes, je suis passionné par l'optimisation des expériences
-              utilisateur. Mon approche est axée sur la modularité, la
+              intéractives, je suis habitué à MongodB . Mon approche est axée sur la modularité, la
               performance, et la résolution efficace des problèmes. Prêt à
-              relever de nouveaux défis, je combine créativité et expertise
+              relever de nouveaux défis et à apprendre de nouvelles techonologies, je combine créativité et expertise
               technique pour offrir des solutions web modernes et intuitives.
             </p>
           </article>
